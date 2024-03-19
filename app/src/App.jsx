0f632c115c08b41f89091293c39acd6daa28a68e
abhi-develope <prajapatiabhishek320@gmail.com>
2024-03-20 05:13:10 +0530

    created navbar and filter section

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,20 +3,70 @@ import styled from "styled-components";
 
 const App = () => {
   return (
-    <MainContainer>
+    <Container>
       <TopContainer>
         <div className="logo">
-          <img src="/logo.png" alt="logo" />
+          <img src="/logo2.png" alt="logo2" />
         </div>
         <div className="search">
-          <input type="search" placeholder="Search Food" />
+          <input placeholder="Search Food" />
         </div>
       </TopContainer>
-    </MainContainer>
+
+     <FilterContainer>
+      <Button>All</Button>
+      <Button>Breakfast</Button>
+      <Button>Lunch</Button>
+      <Button>Dinner</Button>
+      </FilterContainer>
+
+      <FoodContainer>
+        <FoodCards></FoodCards>
+      </FoodContainer>
+    </Container>
   )
 }
 
 export default App
 
-const  MainContainer = styled.div``;
-const TopContainer = styled.section``;
+const  Container = styled.div`
+ max-width: 1200px;
+ margin: 0 auto;
+`;
+const TopContainer = styled.section`
+  min-height: 140px;
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
+  align-items: center;
+
+  .search {
+    input {
+      background-color: transparent;
+      border: 1px solid red;
+      color: white;
+      border-radius: 5px;
+      height: 40px;
+      font-size: 16px;
+      padding: 0 10px;
+    }
+  }
+  `;
+ 
+  const FilterContainer = styled.section`
+     display: flex;
+     justify-content: center;
+     gap: 12px;
+  `
+  const Button = styled.button`
+    background: #351E3B;
+    border-radius: 5px;
+    border: none;
+    padding: 6px 12px;
+    color: white;
+  `
+
+  const FoodContainer = styled.section``
+  const FoodCards = styled.div``
+
+
